80a1d8b2cdc6883a10a2adf61ab2ef12b3d10733
happydayxxl <juergen.lenz@ventrex.com>
2023-11-29 18:21:03 +0100

    added aboutus to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import BackgroundVideo from "@/app/components/backgroundVideo";
+import AboutUs from "@/app/components/aboutUs";
 
 export default function Home() {
 
     return (
         <div>
             <BackgroundVideo />
-
+            <AboutUs />
         </div>
     )
 }
