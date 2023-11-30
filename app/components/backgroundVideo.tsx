f28a3b55613c933b01bc4927ebf60060052d2a05
happydayxxl <juergen.lenz@ventrex.com>
2023-11-30 20:48:50 +0100

    fix autoplay

--- app/components/backgroundVideo.tsx
+++ app/components/backgroundVideo.tsx
@@ -3,15 +3,15 @@ export default function BackgroundVideo() {
 
     return (
         <div className='flex justify-center w-full items-center h-screen'>
-            <video autoPlay muted loop className='absolute inset-0 object-cover w-full h-screen'>
+            <video autoPlay={true} muted loop className='absolute inset-0 object-cover w-full h-screen'>
                 <source src="/video/background_effect.mp4" type="video/mp4"/>
             </video>
 
-                <div className='max-w-[1024px] z-10 justify-center items-center flex flex-col gap-10 mb-20'>
-                    <div className='text-5xl xl:text-7xl z-10'>Len<span className='text-primary'>z</span>o
-                    </div>
-                    <div className='text-3xl xl:text-5xl z-10'>Handel & Consulting</div>
+            <div className='max-w-[1024px] z-10 justify-center items-center flex flex-col gap-10 mb-20'>
+                <div className='text-5xl xl:text-7xl z-10'>Len<span className='text-primary'>z</span>o
                 </div>
+                <div className='text-3xl xl:text-5xl z-10'>Handel & Consulting</div>
+            </div>
 
 
         </div>
