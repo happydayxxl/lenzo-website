530a71b85a639947967916c9a3c4082da7ff4d20
happydayxxl <juergen.lenz@ventrex.com>
2023-11-30 22:11:50 +0100

    added metadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,17 @@ import AboutUs from "@/app/components/aboutUs";
 import Services from "@/app/components/services";
 import Banner from "@/app/components/banner";
 
+
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Lenzo GmbH - Handel und Consulting',
+    description: 'Handel und Beratung von Hygieneprodukten, Reinigungsprodukten und Restaurierungsmaterialien.',
+}
+
 export default function Home() {
+
+
 
     return (
         <div>
